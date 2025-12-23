4fa769ba9a25a12b5889e164c9eac19dea33272a
Alejandro Leal <aleaberm12@gmail.com>
2025-12-23 15:28:57 +0100

    feat(chore): Base app

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
+
+<template>
+  <h1 class="text-red-500">💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+</template>
