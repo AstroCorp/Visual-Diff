b03ef167d6bd2c5bae2499f012b8650dcd53a945
Alejandro Leal <aleaberm12@gmail.com>
2026-01-21 16:21:01 +0100

    action

--- forge.config.ts
+++ forge.config.ts
@@ -54,6 +54,18 @@ const config: ForgeConfig = {
 			[FuseV1Options.OnlyLoadAppFromAsar]: true,
 		}),
 	],
+	publishers: [
+		{
+			name: '@electron-forge/publisher-github',
+			config: {
+				repository: {
+					owner: 'AstroCorp',
+					name: 'Visual-Diff',
+				},
+				prerelease: true,
+			},
+		},
+	],
 };
 
 export default config;
